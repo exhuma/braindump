b3604218cb0ed76980aed7ec224ed56abf60d303
Michel Albert <michel@albert.lu>
2010-06-24 10:41:32 +0200

    Added command line options
     - Ignore "virtual" file systems
     - Restrict to one mount point
     - Ignore symlinks
     - Errors silenced

--- pydu/pydu.py
+++ pydu/pydu.py
@@ -7,7 +7,8 @@ Based on the example shown in the python documentation of "os.walk"
 
 import os
 import sys
-from os.path import join, getsize, isfile
+from os.path import join, getsize, isfile, islink, exists
+from optparse import OptionParser
 
 def human_readable( size ):
    """
@@ -22,32 +23,67 @@ def human_readable( size ):
    elif size < 1024**4:
       return "%1.2f GB" % (size/1024.0**3)
 
-def disk_usage( path, ignored_dirs=[] ):
+def disk_usage( path, ignored_dirs=[], verbose=False ):
    size = 0
+   errors = 0
+
    if isfile( path ):
-      return getsize( path )
+      try:
+         return getsize( path )
+      except OSError, e:
+         errors += 1
+         if verbose:
+            sys.stderr.write( "Unable to determine size for %r (%s)\n" % (
+               join(root, name), str(e)) )
+         return 0, errors
 
    for root, dirs, files in os.walk( path ):
       for name in files:
          try:
             size += getsize(join(root, name))
          except OSError, e:
-            print "Unable to determine size for %r (%s)" % (join(root, name), str(e))
+            if verbose:
+               sys.stderr.write( "Unable to determine size for %r (%s)\n" % (
+                  join(root, name), str(e)) )
+            errors += 1
 
       for idir in ignored_dirs:
          if idir in dirs:
             dirs.remove(idir) # ignore this folder
 
-   return size
+   return size, errors
 
-def get_first_level_sizes( path ):
+def get_first_level_sizes( path, options ):
    """
    Determine the size for each folder and file in <path>
    """
+   mounts = get_mounts()
+   root_mp = get_mountpoint( path, mounts )
+
    print "Calculating ..."
    output = []
    for entry in os.listdir(path):
-      output.append( (join(path, entry), disk_usage( join(path, entry) )) )
+      entry_path = join(path,entry)
+      entry_mp   = get_mountpoint( entry_path, mounts )
+
+      # ignore files on different mountpoints (if enabled)
+      if entry_mp['mountpoint'] != root_mp['mountpoint'] and options.one_fs:
+         continue
+
+      # ignore symbolic links unless forced
+      if islink(entry_path) and not options.follow_symlinks:
+         continue
+
+      # ignore "virtual" mounts
+      if entry_mp['device'] == 'none' and not options.include_virtual:
+         continue
+
+      du, errors = disk_usage( join(path, entry), verbose=options.verbose )
+      output.append((
+         entry_path,
+         du,
+         errors
+         ))
    return output
 
 def pretty_print( results, console_width=80 ):
@@ -72,25 +108,30 @@ def pretty_print( results, console_width=80 ):
    whitespace_len  = 1 # the space after the path name
    whitespace_len += 1 # the space after the path size
    whitespace_len += 2 # the brackets around the prog. bar
+   whitespace_len += 3 # the space and parens around the errors
+
+   # the width of the "errors" text
+   error_len = 9
 
    # remaining console width is filled with the progress bar
-   bar_len = console_width - name_len - size_len - whitespace_len
+   bar_len = console_width - name_len - size_len - error_len - whitespace_len
 
    # sort results by occupied space
    sorted_results = sorted( results, cmp=lambda x, y: cmp(x[1], y[1]))
 
    # the string template for one line
-   line_template = "%%-%ds %%%ds [%%-%ds]" % (name_len, size_len, bar_len)
+   line_template = "%%-%ds %%%ds [%%-%ds] (err: %%4d)" % (name_len, size_len, bar_len)
 
    # for the progress bar
    max_size = sorted_results[-1][1]
 
-   for root, size in sorted_results:
+   for root, size, errors in sorted_results:
       pb_char_count = int(float(size) / max_size * bar_len)
       progress_bar = pb_char_count * "#"
       print line_template % (do_truncate and root[0:40] or root,
             human_readable(size),
-            progress_bar)
+            progress_bar,
+            errors)
 
 def usage():
    print """
@@ -98,12 +139,84 @@ def usage():
       %s <folder_name>
    """ % ( sys.argv[0] )
 
+def get_mounts():
+   """
+   Retrieve a list of mounted volumes
+   So far, only available on linux
+   """
+   if not sys.platform.lower().startswith("linux"):
+      raise UserWarning("Interface to mounted volumes is currently only" \
+            " available on linux!")
+
+   if not exists( "/proc/mounts" ):
+      raise UserWarning("'/proc/mounts' not found!")
+
+   mounts = open("/proc/mounts").readlines()
+   output = {}
+   for line in mounts:
+      device, mountpoint, type, options, fsdump, fspass = line.strip().split()
+      if type == "rootfs":
+         # This seems "ignorable"
+         continue
+
+      output[mountpoint] = {
+         "mountpoint": mountpoint,
+         "device": device,
+         "type": type,
+         "options": options.split(","),
+         "fsdump": fsdump,
+         "fspass": fspass
+      }
+
+   return output
+
+def get_options():
+   usage  = "usage: %prog [options] <folder>"
+   parser = OptionParser(usage=usage)
+   parser.add_option( "-f", "--follow-symlinks", dest="follow_symlinks",
+         help="Include symlinks in the report. By default, this is disabled.",
+         default=False,
+         action="store_true" )
+   parser.add_option( "-1", "--one-fs", dest="one_fs", help="Ignore folders " \
+         "which are mounted on other file systems. For example, if you "      \
+         "report on '/' and '/home' is on a different fs, this option will "  \
+         "prevent it being reported. By default, this is disabled",
+         default=False,
+         action="store_true" )
+   parser.add_option( "-x", "--include-virtual", dest="include_virtual",
+         help="Include virtual file systems. These are filesystems which "    \
+              "have 'none' as mount device (for example: /dev and /proc). "   \
+              "By default this is switched OFF",
+              default=False,
+              action="store_true")
+   parser.add_option( "-v", "--verbose", dest="verbose",
+         help="Print verbose output. Default is OFF",
+         default=False,
+         action="store_true")
+   options, args = parser.parse_args()
+   if not args:
+      parser.print_usage()
+      sys.exit(9)
+   return (options, args)
+
+def get_mountpoint( path, mounts ):
+   """
+   Returns the mountpoint of the specified folder
+
+   @param path: The path
+   @param mounts: A dictionary containing mount information (retrieve it with
+                  get_mounts)
+   """
+   for mountpoint in mounts.keys():
+      if path.startswith( mountpoint ):
+         return mounts[mountpoint]
+
 if __name__ == "__main__":
 
-   if len( sys.argv ) < 2:
-      usage()
-      sys.exit(9)
+   options, args = get_options()
 
-   # pretty_print( get_size( sys.argv[1] ) )
-   pretty_print( get_first_level_sizes( sys.argv[1] ) )
+   pretty_print( get_first_level_sizes( args[0], options ) )
+
+   if not options.verbose:
+      print "NOTE: Enabling the verbose flag will print errors '(err: xxx)' to stderr"
 
