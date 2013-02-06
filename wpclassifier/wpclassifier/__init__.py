bddfd338691706843a7fc4c5b1d7b7e64b205083
Michel Albert <michel@albert.lu>
2013-02-06 08:24:16 +0100

    Implemented new target folder naming scheme.
    Now there are three:
    
    - simple
    - aspect
    - approximate aspect
    
    CLI options changed accordingly.

--- wpclassifier/wpclassifier/__init__.py
+++ wpclassifier/wpclassifier/__init__.py
@@ -4,6 +4,10 @@ import logging
 
 LOG = logging.getLogger(__name__)
 
+F_SIMPLE = 's'
+F_ASPECT = 'a'
+F_APPROX_ASPECT = 'x'
+
 def get_image_size(filepath):
     """
     Returns a 2-tuple of the image dimensions. For example: (1024, 768)
@@ -15,42 +19,93 @@ def get_image_size(filepath):
         LOG.error("Unable to open file %s (%s)" % (filepath, str(e)))
         return None
 
-def simplify(numer, denom):
-    "Simplifies the fraction, returning a tuple"
+def simplify(numer, denom, approximate=False):
+    """
+    Simplifies the fraction, returning a tuple. If ``approximate`` is true,
+    then the resulting fraction is only an approximation of the input number
+    by limiting the denominator of the fraction to ``10``.
+
+    :param numer: The numerator
+    :param denom: The denominator
+    :param approximate: Whether to approximate or not.
+    """
     fract = Fraction(numer, denom)
+    if approximate:
+        fract = fract.limit_denominator(10)
     return fract.numerator, fract.denominator
 
-def aspect_folder(width, height):
+def aspect_folder(width, height, format_=F_SIMPLE):
     """
-    Given an image aspect ratio, return a folder name
+    Given an image aspect ratio, return a folder name.
+
+    :param format_: The folder naming format. One of :py:const:`F_SIMPLE`,
+                   :py:const:`F_ASPECT` or :py:const:`F_APPROX_ASPECT`
     """
-    width, height = simplify(width, height)
+    if format_ == F_SIMPLE:
+        return '{0}x{1}'.format(width, height)
+
+    width, height = simplify(width, height,
+            format_ in (F_ASPECT, F_APPROX_ASPECT))
     return "%d@%d" % (width, height)
 
-def get_image_aspect(filepath):
+def get_image_aspect(filepath, approximate=False):
     """
     Returns the image aspect ratio as a tuple of width/height. This simplifies
     the ratios as much as possible. Which may result in unexpected values.
     For example, instead of (16,10) it will return (8,5). But as this is the
     same value, you can use it as expected
+
+    :param filepath: The filename
+    :param approximate: Whether to approximate the size or not. See
+                        :py:func:`simplify`
     """
     size = get_image_size(filepath)
     if not size:
         return None
-    return simplify(*size)
+    return simplify(*size, approximate=approximate)
 
-def move_files(input_dir, output_dir, as_aspect=False, move=False):
+def move_files(input_dir, output_dir, format_=F_SIMPLE, move=False):
+    """
+    Move files to the classification folders. The source folder is read *non*
+    recursively!
+
+    The names of the target folders depend on the ``format_`` value:
+
+    * F_SIMPLE
+      - A resolution of ``1600x1200`` goes to ``1600x1200``
+
+    * F_ASPECT
+      - A resolution of ``1600x1200`` goes to ``4@3``
+      - A resolution of ``1920x1080`` goes to ``16@9``
+      - A resolution of ``1920x1200`` goes to ``8@5`` (equivalent to
+        ``16@10``)
+      - A resolution of ``1554x1013`` goes to ``1554@1013``
+
+    * F_APPROX_ASPECT
+      - A resolution of ``1600x1200`` goes to ``4@3``
+      - A resolution of ``1920x1080`` goes to ``16@9``
+      - A resolution of ``1920x1200`` goes to ``8@5`` (equivalent to
+        ``16@10``)
+      - A resolution of ``1554x1013`` goes to ``14@9``. See
+        :py:func:`simplify`
+
+    :param input_dir: The directory containing the pictures.
+    :param output_dir: The destination folder.
+    :param format_: The directory naming scheme. Use one of
+                    :py:const:`F_SIMPLE`, :py:const:`F_ASPECT` or
+                    :py:const:`F_APPROX_ASPECT`
+    """
     from os import listdir, makedirs
     from os.path import join, exists, dirname, basename
     from shutil import move as mv, copy2 as cp
-    print as_aspect
     for filename in listdir(input_dir):
         in_file = join(input_dir, filename)
         clazz = None
-        if as_aspect:
-            aspect = get_image_aspect(in_file)
+        if format_ in (F_ASPECT, F_APPROX_ASPECT):
+            approximate = format_ == F_APPROX_ASPECT
+            aspect = get_image_aspect(in_file, approximate)
             if aspect:
-                clazz = aspect_folder(*aspect)
+                clazz = aspect_folder(*aspect, approximate=approximate)
         else:
             size = get_image_size(in_file)
             if size:
--- wpclassifier/wpclassifier/cli.py
+++ wpclassifier/wpclassifier/cli.py
@@ -1,37 +1,75 @@
 import sys
 import logging
-from optparse import OptionParser
+from optparse import OptionParser, IndentedHelpFormatter
 
-from wpclassifier import move_files, aspect_folder
+from wpclassifier import (
+        move_files,
+        aspect_folder,
+        F_APPROX_ASPECT,
+        F_SIMPLE,
+        F_ASPECT)
 
 LOG = logging.getLogger(__name__)
+
+
+class MyHelpFormatter(IndentedHelpFormatter):
+
+    def format_epilog(self, eplg):
+        return eplg
+
 
 def main():
 
     usage = "%prog [options] <input_dir> <output_dir>"
-    parser = OptionParser(usage=usage)
+    parser = OptionParser(usage=usage, formatter=MyHelpFormatter())
     parser.add_option('-v', '--verbose', dest='verbose', action='store_true',
             default=False, help="Be more verbose")
     parser.add_option('-m', '--move', dest='move', action='store_true',
             default=False, help="Instead of copying the files, the files "
             "will be moved to the destination. Default=False")
-    parser.add_option('-a', '--aspect', dest='as_aspect', action='store_true',
-            default=False, help='Instead of using the file resolution as '
-            'folder name, use the aspect ration as folder name. For '
-            'cross-platform compatibility, the generated names will use "@" '
-            'instead of ":" as separator (f. ex.: 16@9) Default: not-set')
-    parser.add_option('-s', '--show-aspect', dest='show_aspect',
-            metavar='SIZE',
-            default=None, help='Show the target folder for a given resolution '
-            'and exit. For example: SIZE=1680x1050')
+    parser.add_option('-f', '--format', dest='format', default=F_SIMPLE,
+            help='Target folder format. Can be one of {0}, {1} or {2}'.format(
+                   F_SIMPLE,
+                   F_ASPECT,
+                   F_APPROX_ASPECT))
+    parser.add_option('-s', '--target-for', dest='target_for',
+            metavar='SIZE', default=None,
+            help='Show the target folder for a given resolution '
+                 'and exit. For example: SIZE=1680x1050')
+    epilog = """\
+
+FORMATS
+=======
+
+There are three different naming options for target folders:
+
+--format=s (the default)
+    When using this format, the application will simply generate folders
+    representing width and height. An image with a width of 1920 and height op
+    1080 will be saved to a folder named '1920x1080'.
+
+--format=a
+    This will store the images into folders which represent the images aspect
+    ratio. Note that the aspect ratios will be aggressively reduced if
+    possible. So an aspect ratio of "16:10" will be saved to "8@5".
+    Additionally, while a proper display would be "16:9", the folder names
+    will be named "16@9" for compatibility with Windows.
+
+--format=x
+    This is the same as ``--format=a`` with the difference that the
+    denominator will never be larger than 10. This results in an approximate
+    destination for files. So the folder "16@9" might contain images which do
+    not quite have this aspect ratio. But it will always be close.
+"""
+    parser.epilog = epilog
 
     (options, args) = parser.parse_args()
 
-    if options.show_aspect:
-        tw, th = options.show_aspect.split('x')
-        target = aspect_folder(int(tw), int(th))
+    if options.target_for:
+        tw, th = options.target_for.split('x')
+        target = aspect_folder(int(tw), int(th), options.format)
         print "Pictures of size %s will be moved into %s" % (
-                options.show_aspect, target)
+                options.target_for, target)
         sys.exit(0)
 
     if len(args) != 2:
@@ -46,4 +84,4 @@ def main():
 
     input_dir, output_dir = args
 
-    move_files(input_dir, output_dir, options.as_aspect, move=options.move)
+    move_files(input_dir, output_dir, options.format, move=options.move)
