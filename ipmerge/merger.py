4fe8713102478d7aa253265547ec0fc69d8f38a2
Michel Albert <michel@albert.lu>
2014-02-28 10:43:18 +0100

    Improved code readability.

--- ipmerge/merger.py
+++ ipmerge/merger.py
@@ -9,26 +9,25 @@ def merge(networks):
             output.append(net)
             continue
 
-        # Keep a ref to output[-1]
-        bucket = output.pop()
+        # Keep a ref to output[-1] (just to keep the code more readable)
+        head = output[-1]
 
         # If the current net is completely contained inside the last item on
         # the stack we can simply ignore it.
-        if (bucket.network_address <= net.network_address and
-                bucket.broadcast_address >= net.broadcast_address):
-            output.append(bucket)
+        if (head.network_address <= net.network_address and
+                head.broadcast_address >= net.broadcast_address):
+            continue
 
         # If the current net does not have the same prefix length, we cannot
-        # merge them. Put both back on the stack.
-        elif net.prefixlen != bucket.prefixlen:
-            output.append(bucket)
+        # merge them. Shift it onto the stack.
+        elif net.prefixlen != head.prefixlen:
             output.append(net)
 
         # If the network which immediately follows last item on the stack is
-        # the same as the current net, we can merge (i.e. add the buckets
+        # the same as the current net, we can merge (i.e. add the head's
         # supernet)
-        elif list(bucket.supernet().subnets())[1] == net:
-            output.append(bucket.supernet())
+        elif list(head.supernet().subnets())[1] == net:
+            output[-1] = head.supernet()
 
             # Now, the last item on the stack changed, and possibly this can
             # have an effect on previous items on the stack. Reduce the stack
